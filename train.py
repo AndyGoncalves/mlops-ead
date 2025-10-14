2086bdbc590701b4c6d54d8784f5309795ce86c8
Andréia Gonçalves <andreiagoncalves.ufmg@gmail.com>
2025-10-14 16:31:59 -0300

    add source files

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
+
 """# 1 - Importando os módulos necessários"""
 
 import os
 import random
-import mlflow
 import numpy as np
+import mlflow
 import random as python_random
 import tensorflow
 import tensorflow as tf
@@ -21,20 +21,37 @@ from sklearn.model_selection import train_test_split
 
 """# Definindo funções adicionais"""
 
-def reset_seeds():
-   os.environ['PYTHONHASHSEED']=str(42)
-   tf.random.set_seed(42)
-   np.random.seed(42)
-   random.seed(42)
+def reset_seeds() -> None:
+  """
+  Resets the seeds to ensure reproducibility of results.
+
+  This function sets the seed for various random number generation libraries
+  to ensure that results are reproducible. The affected libraries are:
+  - Python's built-in `random`
+  - NumPy
+  - TensorFlow
+
+  The seed used is 42.
+
+  Returns:
+      None
+  """
+  os.environ['PYTHONHASHSEED']=str(42)
+  tf.random.set_seed(42)
+  np.random.seed(42)
+  random.seed(42)
 
 """# 2 - Fazendo a leitura do dataset e atribuindo às respectivas variáveis"""
 
-data = pd.read_csv('https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
+data = pd.read_csv(f'https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
+
+data.to_csv('fetal_health_reduced.csv', index=False)
+
 
 """# 3 - Preparando o dado antes de iniciar o treino do modelo"""
 
-X=data.drop(["fetal_health"], axis=1)
-y=data["fetal_health"]
+X = data.drop(["fetal_health"], axis=1)
+y = data["fetal_health"]
 
 columns_names = list(X.columns)
 scaler = preprocessing.StandardScaler()
@@ -54,9 +71,9 @@ y_test = y_test - 1
 reset_seeds()
 model = Sequential()
 model.add(InputLayer(input_shape=(X_train.shape[1], )))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(3, activation='softmax'))
+model.add(Dense(units=10, activation='relu'))
+model.add(Dense(units=10, activation='relu'))
+model.add(Dense(units=3, activation='softmax'))
 
 """# 5 - Compilando o modelo
 
@@ -66,20 +83,25 @@ model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
+"""##**Configurando o mlflow**"""
 
-os.environ['MLFLOW_TRACKING_USERNAME'] = 'renansantosmendes'
-os.environ['MLFLOW_TRACKING_PASSWORD'] = '6d730ef4a90b1caf28fbb01e5748f0874fda6077'
-mlflow.set_tracking_uri('https://dagshub.com/renansantosmendes/mlops-ead.mlflow')
+
+
+os.environ['MLFLOW_TRACKING_USERNAME'] = 'AndyGoncalves'
+os.environ['MLFLOW_TRACKING_PASSWORD'] = '1d7f8459c2cdd075a0317f26fc322caf353acdf4'
+mlflow.set_tracking_uri('https://dagshub.com/AndyGoncalves/mlops-ead.mlflow')
 
 mlflow.tensorflow.autolog(log_models=True,
                           log_input_examples=True,
                           log_model_signatures=True)
 
+
 """# 6 - Executando o treino do modelo"""
 
-with mlflow.start_run(run_name='experiment_andy_mlops_ead') as run:
+with mlflow.start_run(run_name='experiment_mlops_ead') as run:
   model.fit(X_train,
             y_train,
             epochs=50,
             validation_split=0.2,
             verbose=3)
+
